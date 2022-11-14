8aefcff2390c7334f190dcb4a205ed50726ede80
Graimi <jaimefloreslujan@gmail.com>
2022-11-14 09:48:22 +0100

    how to create variables

---
+++ 01-variables.js
@@ -0,0 +1,37 @@
+var greetings = 'hola desde'
+var school = 'pmba'
+
+var greetingsFromSchool = greetings + ' ' + school
+
+console.log(
+    greetings + ' ' + school
+)
+
+console.log(
+    greetingsFromSchool
+)
+
+// Opción A, para variables en general, más frecuente
+// holaQueTal (camelCase)
+
+// Opción B, para variables generales que no cambian, menos frecuente
+// FINAL_TEXT (PASCAL_CASE)
+
+/* 
+String
+Number
+Boolean
+Array
+Object
+*/
+
+'text'
+"text"
+
+var ageAsString = '30'
+var ageAsNumber = 30
+var arrayOfValues = [30, '30', false, true, 4]
+var objectOfValues = {
+    age: 30,
+    ageAs: ageAsString
+}
